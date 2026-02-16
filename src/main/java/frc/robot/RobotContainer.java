70165e9bb66ddfe500ae40165e8a5938c4144dda
Ed Peters <epeters1111@gmail.com>
2026-02-16 14:26:26 -0500

    Reformats main classes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,45 +14,45 @@ import frc.robot.util.CommandLogger;
 
 public class RobotContainer {
 
-  final GameController driver = new GameController(0);
-  final GameController operator = new GameController(1);
+    final GameController driver = new GameController(0);
+    final GameController operator = new GameController(1);
 
-  final CommandSwerveDrivetrain drivetrain;
-  final SwerveSubsystem swerve;
-  final LimelightSubsystem limelight;
-  final AutonomousSubsystem auto;
+    final CommandSwerveDrivetrain drivetrain;
+    final SwerveSubsystem swerve;
+    final LimelightSubsystem limelight;
+    final AutonomousSubsystem auto;
 
-  public RobotContainer() {
+    public RobotContainer() {
 
-    CommandLogger.enable();
-    CommandLogger.addController("Driver", driver);
-    CommandLogger.addController("Operator", operator);
+        CommandLogger.enable();
+        CommandLogger.addController("Driver", driver);
+        CommandLogger.addController("Operator", operator);
 
-    // create CTRE drivetrain and wrap it in SwerveSubsystem
-    drivetrain = TunerConstants.createDrivetrain();
-    swerve = new SwerveSubsystem(drivetrain);
-    limelight = new LimelightSubsystem(swerve);
-    auto = new AutonomousSubsystem(swerve);
+        // create CTRE drivetrain and wrap it in SwerveSubsystem
+        drivetrain = TunerConstants.createDrivetrain();
+        swerve = new SwerveSubsystem(drivetrain);
+        limelight = new LimelightSubsystem(swerve);
+        auto = new AutonomousSubsystem(swerve);
 
-    // configure driver controls
-    configureBindings();
-  }
+        // configure driver controls
+        configureBindings();
+    }
 
-  private void configureBindings() {
+    private void configureBindings() {
 
-    // default command: normal teleop drive
-    swerve.setDefaultCommand(swerve.driveCommand(driver));
+        // default command: normal teleop drive
+        swerve.setDefaultCommand(swerve.driveCommand(driver));
 
-    // hold left bumper for orbit mode (face and orbit around target)
-    driver.leftBumper()
-        .whileTrue(swerve.orbitCommand(driver));
+        // hold left bumper for orbit mode (face and orbit around target)
+        driver.leftBumper()
+                .whileTrue(swerve.orbitCommand(driver));
 
-    // zero pose on left click, accept vision pose on right click
-    driver.leftStick().onTrue(swerve.zeroPoseCommand());
-    driver.rightStick().onTrue(limelight.resetPoseFromVisionCommand());
-  }
+        // zero pose on left click, accept vision pose on right click
+        driver.leftStick().onTrue(swerve.zeroPoseCommand());
+        driver.rightStick().onTrue(limelight.resetPoseFromVisionCommand());
+    }
 
-  public Command getAutonomousCommand() {
-    return auto.generateCommand();
-  }
+    public Command getAutonomousCommand() {
+        return auto.generateCommand();
+    }
 }
