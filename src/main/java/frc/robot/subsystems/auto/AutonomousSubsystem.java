d529e094cbb3fa1556f129f5b0625dc3e0516a43
TeamRobohawk02 <198489499+TeamRobohawk@users.noreply.github.com>
2026-03-21 14:55:38 -0400

    Fix
    It works

--- src/main/java/frc/robot/subsystems/auto/AutonomousSubsystem.java
+++ src/main/java/frc/robot/subsystems/auto/AutonomousSubsystem.java
@@ -244,7 +244,7 @@ public class AutonomousSubsystem extends SubsystemBase {
         programs.put("RIZZ", "DEPB");
 
         // Bethesda
-        programs.put("MID", "HUBSL");
+        //programs.put("MID", "HUBSL");
         return programs;
     }
 
